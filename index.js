5b8d229a08d54f38805c8d6eb393e5979dd3498b
ImFaisalKhalid <faisalkhalid246@gmail.com>
2021-04-03 21:38:54 -0500

    Added event handling

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Discord.Client();
+
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
+
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
+
+for (const file of eventFiles) {
+  const event = require(`./Events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.DISCORD_TOKEN);
