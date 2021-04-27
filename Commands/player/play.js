cd37e63c3dfbb244ebd972bc79e7cafc9e941f6e
ImFaisalKhalid <faisalkhalid246@gmail.com>
2021-04-26 22:01:35 -0500

    Made play a bit more arg friendly

--- Commands/player/play.js
+++ Commands/player/play.js
@@ -1,15 +1,10 @@
 const ytdl = require('ytdl-core-discord');
 const dotenv = require('dotenv');
 const { google } = require('googleapis');
-const SpotifyWebApi = require('spotify-web-api-node');
 const config = require('../../config.json');
-const authorizeServer = require('../spotify/utils/authenticate-server');
 
 // Configure the .env
 dotenv.config();
-
-// Create our api wrapper
-const spotifyApi = new SpotifyWebApi();
 
 // Create our Youtube wrapper
 const youtubeV3 = google.youtube({ version: 'v3', auth: process.env.YOUTUBE_KEY });
@@ -39,7 +34,7 @@ async function play(client, message, data) {
   }
 
   // eslint-disable-next-line no-param-reassign
-  data.dispatcher = await data.connection.play(await ytdl(data.queue[0].url), { type: 'opus', bitrate: 512000, highWaterMark: 30 });
+  data.dispatcher = await data.connection.play(await ytdl(data.queue[0].url), { type: 'opus', bitrate: 512000, highWaterMark: 50, volume: 0.5 });
   // eslint-disable-next-line no-param-reassign
   data.dispatcher.guildId = data.guildId;
 
@@ -95,12 +90,6 @@ module.exports = {
   args: true,
   cooldown: 1,
   async execute(message, args, mongoClient, client) {
-    // Arg check
-    if (args[0] === undefined || args[1] === undefined) {
-      message.channel.send(config.CHECK_USAGE_ERROR);
-      return;
-    }
-
     // Check if user is in a voice channel
     if (!message.member.voice.channel) {
       message.channel.send(config.VOICE_CHAT_ERROR);
@@ -113,44 +102,6 @@ module.exports = {
       message.channel.send(config.ALREADY_IN_VC_ERROR);
       return;
     }
-
-    // Arg handling
-    const trackName = args[0].replace(/-/g, ' ');
-    const artistName = args[1].replace(/-/g, ' ');
-    const query = `track:${trackName} artist:${artistName}`;
-
-    // Connect to database and get server info
-    const myDb = mongoClient.db(message.guild.id.toString());
-    const collection = myDb.collection('server-info');
-
-    // Reauthorize the server and get a new token
-    await authorizeServer.execute(message, mongoClient);
-
-    // Use server info to get authorization token
-    let serverToken = '';
-    await collection.find().forEach(
-      (myDoc) => {
-        if (myDoc.name === message.guild.name) {
-          // Set our access token
-          serverToken = myDoc.serverToken;
-        }
-      },
-    );
-    await spotifyApi.setAccessToken(serverToken);
-
-    // Search for song based off query
-    let song = '';
-    let artist = '';
-    await spotifyApi.searchTracks(query)
-      .then((data) => {
-        song = data.body.tracks.items[0].name;
-        artist = data.body.tracks.items[0].artists[0].name;
-      })
-      .catch((err) => {
-        console.log(err);
-        message.channel.send(config.SONG_SEARCH_ERROR);
-        return;
-      });
 
     // Get the active collection. If none, make one
     const data = client.active.get(message.guild.id) || {};
@@ -168,19 +119,55 @@ module.exports = {
     // Set this data to our specific guild
     data.guildId = message.guild.id;
 
+    // Arg handling
+    if (args[0].includes('youtube.com')) {
+      // No API call. Go direct to ytdl
+      await data.queue.push({
+        songTitle: 'Youtube',
+        songArtist: 'direct URL',
+        requester: message.author.tag,
+        url: args[0],
+      });
+
+      if (!data.dispatcher) {
+        play(client, message, data);
+      } else {
+        message.channel.send('Added to queue! Here is the queue now:');
+        for (let i = 0; i < data.queue.length; i += 1) {
+          const messageData = [];
+          messageData.push('------------------------------------------');
+          if (i === 0) {
+            messageData.push('***NOW PLAYING***');
+          }
+          messageData.push(`***Song:*** ${data.queue[i].songTitle}`);
+          messageData.push(`***By:*** ${data.queue[i].songArtist}`);
+          messageData.push(`***Requester:*** ${data.queue[i].requester}`);
+
+          message.channel.send(messageData);
+        }
+      }
+
+      // Set our map
+      client.active.set(message.guild.id, data);
+
+      return;
+    }
+
+    console.log(args.join(' '));
+
     // Call to youtube api
     await youtubeV3.search.list({
       part: 'snippet',
       type: 'video',
-      q: `${song} ${artist} lyrics`,
+      q: args.join(' '),
       maxResults: 5,
       order: 'relevance',
       safeSearch: 'None',
     }, async (err, response) => {
       // Add data to our queue
       data.queue.push({
-        songTitle: song,
-        songArtist: artist,
+        songTitle: response.data.items[0].snippet.title,
+        songArtist: response.data.items[0].snippet.channelTitle,
         requester: message.author.tag,
         url: `https://youtube.com/watch?v=${response.data.items[0].id.videoId}`,
       });
